f886e94f003434617fdde78d0712cf65a52e043a
TepChoulong <tepchoulongzin@gmail.com>
2023-07-09 19:47:12 +0700

    First Commit

---
+++ List/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+/*
+    List = data structure that represents a list of objects that
+           can be accessed by index. Similar to array, but can
+           dynamically increase/decrease in size using System.Collection.Generic;
+*/
+
+int index;
+
+List<String> Cars = new List<string>();
+
+Cars.Add("Ford Mustang");
+Cars.Add("Toyota Camry");
+Cars.Add("Toyota Prius");
+Cars.Add("Toyota Innova C");
+
+Cars.Remove("Toyota Innova C");
+Cars.Insert(3, "Ford Eco Sport");
+
+
+
+foreach(String car in Cars)
+{
+    index = Cars.IndexOf(car);
+
+    Console.WriteLine($"{index}. {car}");
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+polymorphism = Greek word that means to "have many forms"
+               Objects can be identified by more than one type
+               Ex. A Dog is also: Canine, Animal, Organism
+*/
+
+Apple apple = new Apple();
+Banana banana = new Banana();
+Pineapple pineapple = new Pineapple();
+
+Fruit[] fruits = {apple, banana, pineapple};
+
+foreach(Fruit fruit in fruits)
+{
+    // fruit.Sweet(); // This apple is sweet, This banana is sweet, This pineapple is sweet.
+    // fruit.Tart(); // This pineapple is tart.
+    fruit.Sour(); // This apple is sweet, This pineapple is sour.
+}
+
+class Fruit
+{
+    public virtual void Sweet()
+    {
+
+    }
+    public virtual void Sour()
+    {
+
+    }
+    public virtual void Tart()
+    {
+
+    }
+}
+class Apple:Fruit
+{
+    public override void Sweet()
+    {
+        Console.WriteLine("This apple is sweet.");
+    }
+    public override void Sour()
+    {
+        Console.WriteLine("This apple is sweet.");
+    }
+}
+class Banana:Fruit
+{
+    public override void Sweet()
+    {
+        Console.WriteLine("This banana is sweet.");
+    }
+}
+class Pineapple:Fruit
+{
+    public override void Sweet()
+    {
+        Console.WriteLine("This pineapple is sweet.");
+    }
+    public override void Sour()
+    {
+        Console.WriteLine("This pineapple is sour.");
+    }
+    public override void Tart()
+    {
+        Console.WriteLine("This pineapple is tart.");
+    }
+}
